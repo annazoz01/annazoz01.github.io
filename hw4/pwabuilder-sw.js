f4653362ebfc81640cf67907faeaee18455c9b64
annazoz01 <58395412+annazoz01@users.noreply.github.com>
2020-04-26 15:12:02 +0300

    Update pwabuilder-sw.js

--- hw4/pwabuilder-sw.js
+++ hw4/pwabuilder-sw.js
@@ -19,9 +19,6 @@ self.addEventListener('install', async (event) => {
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
