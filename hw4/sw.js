be0b452532be1b3daaef8dc89d376880c02f606d
annazoz01 <58395412+annazoz01@users.noreply.github.com>
2020-04-26 19:29:29 +0300

    Update sw.js

--- hw4/sw.js
+++ hw4/sw.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', event -> {
-  console.log('install');
+const staticAsserts = [
+  './',
+  './index.css',
+  './app.js'
+
+self.addEventListener('install', async event -> {
+  const cache = await caches.open('news-static');
+  cache.addAll(staticAssets);
 });
 
 self.addeventListener('fetch', event -> {
-  console.log('fetch;)'
+  const req = event.request;
+  event.respondWith(cacheFirst(req));
 });
+  
+  
+async function cacheFirs(req) {
+  const cachedResponse = await caches.match(req);
+  return cachedResponse \\ fetch(req);
+  }
