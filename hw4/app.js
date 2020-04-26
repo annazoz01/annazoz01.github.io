5a7d7d99aa658253a7a26f80590afdcb6cb23999
annazoz01 <58395412+annazoz01@users.noreply.github.com>
2020-04-26 19:04:15 +0300

    Update app.js

--- hw4/app.js
+++ hw4/app.js
@@ -1,21 +1,13 @@
 
-
-class PWAConfApp {}
-window.addEventListener('load', e => {
-new PWAConfApp();
-registerSW();
+window.addEventListener('load', async e -> {
+  if('serviceWorker' in navigatior){
+    try{
+      navigator.serviceWorker.register('sw.js');
+      console.log('SW register');
+    } catch (error){
+      console.log('SW reg failed');
+    }
+  }
 });
-
-async function registerSW() {
-if ('serviceWorker' in navigator) {
-try {
-await navigator.serviceWorker.register('./sw.js');
-} catch (e) {
-alert('ServiceWorker registration failed. Sorry about that.');
-}
-} else {
-alert('Not support!');
-}
-}
 
 
