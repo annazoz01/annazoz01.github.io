b6380aa11e098d3941df7dceb2cf550553e30d02
annazoz01 <58395412+annazoz01@users.noreply.github.com>
2020-04-27 11:04:19 +0300

    Update sw.js

--- hw4/sw.js
+++ hw4/sw.js
@@ -1,32 +1,46 @@
-self.addEventListener('install', function(event) {
-  var indexPage = new Request('index.html');
-  event.waitUntil(
-    fetch(indexPage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
-        return cache.put(indexPage, response);
-      });
-  }));
-});
-self.addEventListener('fetch', function(event) {
-  var updateCache = function(request){
-    return caches.open('pwabuilder-offline').then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
-        return cache.put(request, response);
-      });
+function registerServiceWorker() {
+// регистрирует скрипт sw в поддерживаемых браузерах
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
+      console.log('Service Worker registered successfully.');
+    }).catch(error => {
+      console.log('Service Worker registration failed:', error);
     });
-  };
-  event.waitUntil(updateCache(event.request));
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-      return caches.open('pwabuilder-offline').then(function (cache) {
-        return cache.match(event.request).then(function (matching) {
-          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report
-        });
-      });
-    })
-  );
-})
+  }
+}
+
+// sw.js
+self.addEventListener('install', e => {
+ e.waitUntil(
+   // после установки service worker
+   // открыть новый кэш
+   caches.open('my-pwa-cache').then(cache => {
+     // добавляем все URL ресурсов, которые хотим закэшировать
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/about.html',
+       '/images/doggo.jpg',
+       '/styles/main.min.css',
+       '/scripts/main.min.js',
+     ]);
+   })
+ );
+});
+
+
+import gulp from 'gulp';
+import path from 'path';
+import swPrecache from 'sw-precache';
+
+const rootDir = '/';
+
+gulp.task('generate-service-worker', callback => {
+  swPrecache.write(path.join(rootDir, 'sw.js'), {
+    staticFileGlobs: [
+      // отслеживание и кэширование всех файлов для сравнения с этим шаблоном
+      rootDir + '/**/*.{js,html,css,png,jpg,gif}',
+    ],
+    stripPrefix: rootDir
+  }, callback);
+});
